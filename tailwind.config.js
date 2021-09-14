ab92089862b9dc079c46016c7ff174fd73869579
saranganES <43719998+saranganES@users.noreply.github.com>
2021-09-14 19:36:25 +0530

    apt-eat-site completed

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      boxShadow: {
+        pink: "0 4px 14px 0 #FFEBD0",
+      },
+      fontFamily: {
+        body: ["Nunito"],
+        Mont: ["Montserrat"],
+      },
+      maxWidth: {
+        custom: "80%",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
